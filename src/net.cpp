036943d06095d144af026b68d54a3f383b18168d
zero24x <zero@you-spam.com>
2018-03-31 15:51:19 +0200

    Logo updated, added sync infos.

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"syncnode-1.variuscoin.cf", "syncnode-2.variuscoin.cf"},
+    {"syncnode-1.variusproject.ml", "syncnode-1.variusproject.ml"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
