00ff5d5d2958de1ee2022d7e47d967bfb4411e58
zero24x <zero@you-spam.com>
2018-04-06 22:40:37 +0200

    Build with ipv6 support

--- build_qt_static.bat
+++ build_qt_static.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;C:\Qt\485\bin
 cd C:\Users\wolfgang\Documents\Privat\Coins\Dev\Varius
-qmake "USE_UPNP=1" VARIUS-qt.pro
+qmake "USE_UPNP=1" "USE_IPV6=1
 mingw32-make -f Makefile.Release
 pause
