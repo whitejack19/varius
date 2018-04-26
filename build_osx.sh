34e66cc2eaa8176a4d012b1ce39c6d9e2adf2e98
admin <admin@admins-Mac.local>
2018-04-27 00:21:11 +0200

    mac: better naming

--- build_osx.sh
+++ build_osx.sh
@@ -2,4 +2,4 @@ qmake RELEASE=1 USE_UPNP=1 USE_QRCODE=0 VARIUS-qt.pro
 make
 export QTDIR=/usr/local/Cellar/qt/5.10.1/
 T=$(contrib/qt_translations.py $QTDIR/translations src/qt/locale)
-python2.7 contrib/macdeploy/macdeployqtplus  -add-qt-tr $T -dmg -fancy contrib/macdeploy/fancy.plist VARIUS-Qt.app
+python2.7 contrib/macdeploy/macdeployqtplus  -add-qt-tr $T -dmg -fancy contrib/macdeploy/fancy.plist VARIUS.app
